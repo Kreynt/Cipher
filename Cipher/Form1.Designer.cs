de3436f52658cf54780d419549bad93ab22d50fe
Александр Ларин <alex175alex@outlook.com>
2020-12-19 12:54:53 +0300

    Первый уровень закрытого шифрования

--- Cipher/Form1.Designer.cs
+++ Cipher/Form1.Designer.cs
@@ -32,7 +32,6 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -68,23 +67,13 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(41, 294);
+            this.textBox4.Location = new System.Drawing.Point(43, 676);
             this.textBox4.MaxLength = 999999999;
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox4.Size = new System.Drawing.Size(450, 184);
-            this.textBox4.TabIndex = 4;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(43, 676);
-            this.textBox5.MaxLength = 999999999;
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox5.Size = new System.Drawing.Size(450, 84);
-            this.textBox5.TabIndex = 5;
+            this.textBox4.Size = new System.Drawing.Size(450, 84);
+            this.textBox4.TabIndex = 5;
             // 
             // button1
             // 
@@ -102,7 +91,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(533, 772);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -120,7 +108,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
     }
 }
--- Cipher/Form1.cs
+++ Cipher/Form1.cs
@@ -17,9 +17,26 @@ namespace Cipher
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//Названия переменных, объектов и методов сделаны такими специально, обычно я их нормально называю :-) 
         {
+            string s = textBox1.Text;
+            for (int j = 0; j < s.Length - 2; j += 2)
+            {
+                char ch = s[j];
+                s = s.Remove(j, 1);
+                s = s.Insert(j + 1, ch.ToString());
+            }
 
+            textBox2.Text = s;
+
+            for (int j = 0; j < s.Length - 2; j += 2)
+            {
+                char ch = s[j];
+                s = s.Remove(j, 1);
+                s = s.Insert(j + 1, ch.ToString());
+            }
+
+            textBox3.Text = s;
         }
     }
 }
